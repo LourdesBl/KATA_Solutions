e8806c8ac10f7c10c9e78a10d8a16a69c081e459
lourdes.bloin <lourdes.bloin@aent.com>
2018-07-29 18:51:54 -0300

    Id Transformation

--- Kata/Program.cs
+++ Kata/Program.cs
@@ -22,44 +22,48 @@ namespace Kata
 
 		private void Run()
 		{
-			int adid = 543307;
-			//Console.WriteLine("543307Z --" + _moreSolutions.ConvertCharToNumber(adid, "543307Z"));
+			int adid = 543347;
+			var regex = Regex.Matches("543347AAB", @"([A-Z])");
+			Console.WriteLine(int.Parse(regex.Count == 0 ? "543347AAB" : "543347AAB".Substring(0, "543347AAB".Length - regex.Count)));
+			Console.WriteLine("543347A --" + _moreSolutions.ConvertCharToNumber(adid, "543347A"));
+			Console.WriteLine("543347B --" + _moreSolutions.ConvertCharToNumber(adid, "543347B"));
+			Console.WriteLine("543347C --" + _moreSolutions.ConvertCharToNumber(adid, "543347C"));
 			//Console.WriteLine("543307AA --" + _moreSolutions.ConvertCharToNumber(adid, "543307AA"));
 			//Console.WriteLine("543307AZ --" + _moreSolutions.ConvertCharToNumber(adid, "543307AZ"));
 			//Console.WriteLine("543307BA --" + _moreSolutions.ConvertCharToNumber(adid, "543307BA"));
 			//Console.WriteLine("543307BZ --" + _moreSolutions.ConvertCharToNumber(adid, "543307BZ"));
 			//Console.WriteLine("543307AAA --" + _moreSolutions.ConvertCharToNumber(adid, "543307AAA"));
 			//Console.WriteLine("543307AZA --" + _moreSolutions.ConvertCharToNumber(adid, "543307AZA"));
-			string id = Console.ReadLine();
-			while (!string.IsNullOrEmpty(id))
-			{
-				Console.WriteLine(id + "--" + _moreSolutions.ConvertCharToNumber(adid, id));
-				id = Console.ReadLine();
-			}
+			//string id = Console.ReadLine();
+			//while (!string.IsNullOrEmpty(id))
+			//{
+			//	Console.WriteLine(id + "--" + _moreSolutions.ConvertCharToNumber(adid, id));
+			//	id = Console.ReadLine();
+			//}
 
-			var result = _kataSolutions.SqInRect(5, 3);
-			Console.Write('{');
-			result.ForEach(x => Console.Write(x + ","));
-			Console.WriteLine('}');
+			//var result = _kataSolutions.SqInRect(5, 3);
+			//Console.Write('{');
+			//result.ForEach(x => Console.Write(x + ","));
+			//Console.WriteLine('}');
 
-			result = _kataSolutions.SqInRect(90, 5);
-			Console.Write('{');
-			result?.ForEach(x => Console.Write(x + ","));
-			Console.Write('}');
+			//result = _kataSolutions.SqInRect(90, 5);
+			//Console.Write('{');
+			//result?.ForEach(x => Console.Write(x + ","));
+			//Console.Write('}');
 
-			//int[]  peopleInLine = new[] { 25, 50,50, 100 };
-			//Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
-			//peopleInLine = new[] { 25, 50, 50, 100,100 };
-			//Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
-			//peopleInLine = new[] { 25, 50, 50, 100,50,25 };
-			//Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
-			//Console.WriteLine(_kataSolutions.GetSum(0,4));
-			//Console.WriteLine(_kataSolutions.Solution("MDCLXVI"));
-			int[] arr = { 34, 15, 88, 2 };
-			//Console.WriteLine(_kataSolutions.FindShort("bitcoin take over the world maybe who knows perhaps"));
-			//_kataSolutions.AlphabetPosition("The sunset sets at twelve o' clock.");
-			//Console.WriteLine(_kataSolutions.PrinterError("aaaxbbbbyyhwawiwjjjwwm"));
-			//Console.WriteLine(_kataSolutions.PrinterError("aaabbbbhaijjjm"));
+			////int[]  peopleInLine = new[] { 25, 50,50, 100 };
+			////Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
+			////peopleInLine = new[] { 25, 50, 50, 100,100 };
+			////Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
+			////peopleInLine = new[] { 25, 50, 50, 100,50,25 };
+			////Console.WriteLine(_kataSolutions.Tickets(peopleInLine));
+			////Console.WriteLine(_kataSolutions.GetSum(0,4));
+			////Console.WriteLine(_kataSolutions.Solution("MDCLXVI"));
+			//int[] arr = { 34, 15, 88, 2 };
+			////Console.WriteLine(_kataSolutions.FindShort("bitcoin take over the world maybe who knows perhaps"));
+			////_kataSolutions.AlphabetPosition("The sunset sets at twelve o' clock.");
+			////Console.WriteLine(_kataSolutions.PrinterError("aaaxbbbbyyhwawiwjjjwwm"));
+			////Console.WriteLine(_kataSolutions.PrinterError("aaabbbbhaijjjm"));
 			Console.ReadLine();
 		}
 
